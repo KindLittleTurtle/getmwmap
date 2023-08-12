985c45d52b90292628aa7d8e8554c1dfa108ac2c
核善的小兲 <hsdxt@mwtour.cn>
2023-08-12 14:11:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='getmwmap',
-    version='1.0.0',
+    version='1.0.1',
     packages=['getmwmap'],
     url='https://github.com/KindLittleTurtle/getmwmap',
     license='AGPL3.0',
     author='MWCTTO',
     author_email='to@mwtour.cn',
-    description='这个人很懒，没有留下什么',
-    long_description=open(r'G:\getmwmap\README.md', encoding='utf-8').read(),
+    description='为MW锦标赛提高快速随机抽取地图的拓展',
+    long_description=open(r'C:\getmwmap\README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown'
 )
