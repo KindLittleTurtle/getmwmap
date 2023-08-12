b7281a51e0ec82d64075ad06165d59e5f2d4d864
核善的小兲 <hsdxt@mwtour.cn>
2023-08-12 12:01:05 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='getmwmap',
+    version='1.0.0',
+    packages=['getmwmap'],
+    url='',
+    license='AGPL3.0',
+    author='MWCTTO',
+    author_email='to@mwtour.cn',
+    description='这个人很懒，没有留下什么',
+    long_description=open(r'G:\getmwmap\README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown'
+)
