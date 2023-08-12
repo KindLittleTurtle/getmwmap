eeea07dbb4fda78809ba7aadfb643eb564ab8f51
核善的小兲 <hsdxt@mwtour.cn>
2023-08-12 21:28:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='getmwmap',
-    version='1.0.1',
+    version='1.0.2',
     packages=['getmwmap'],
     url='https://github.com/KindLittleTurtle/getmwmap',
     license='AGPL3.0',
